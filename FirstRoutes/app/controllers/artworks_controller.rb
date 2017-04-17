56cca1bf9b7dfcf805c6304ddb20c2b35d362bb4
App Academy Student <student@appacademy.io>
2017-04-17 16:38:36 -0700

    Complete users and artworks api phase

---
+++ FirstRoutes/app/controllers/artworks_controller.rb
@@ -0,0 +1,51 @@
+class ArtworksController < ApplicationController
+  def index
+    @artworks = Artwork.all
+    render json: @artworks
+  end
+
+  def create
+    artwork = Artwork.new(artwork_params)
+
+    if artwork.save
+      render json: artwork
+    else
+      render(
+        json: artwork.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def show
+    artwork = Artwork.find_by(id: params[:id])
+    render json: artwork
+  end
+
+  def update
+    artwork = Artwork.find_by(id: params[:id])
+    if artwork.update(artwork_params)
+      render json: artwork
+    else
+      render(
+        json: artwork.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def destroy
+    artwork = Artwork.find_by(id: params[:id])
+    if artwork.delete
+      render json: artwork
+    else
+      render(
+        json: artwork.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
+  end
+end
